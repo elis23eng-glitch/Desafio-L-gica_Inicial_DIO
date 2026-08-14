3615a534e5617494b67c587ce5600df2b7e4f25b
elis23eng-glitch <elis23.eng@gmail.com>
2026-08-14 20:57:42 -0300

    Atualiza Index.js

--- Index.js
+++ Index.js
@@ -2,4 +2,10 @@
 
 =======
 console.log("Olá comunidade Tech");
+<<<<<<< HEAD
 >>>>>>> 48e626c (meu primeiro desafio de Lógica)
+=======
+console.log("Projeto Desafio de Lógica Inicial DIO")
+
+
+>>>>>>> 48f5da1 (Atualiza Index.js)
